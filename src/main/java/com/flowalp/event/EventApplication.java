b8373965c49907b45dcc192f15a46255d5282f56
francesco.ranghetti <francesco.ranghetti@suntis.ch>
2025-06-25 00:21:55 +0200

    feat(cleanup-and-structure): created bean for modelmapper

--- src/main/java/com/flowalp/event/EventApplication.java
+++ src/main/java/com/flowalp/event/EventApplication.java
@@ -1,7 +1,9 @@
 package com.flowalp.event;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,8 +12,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration(proxyBeanMethods = false)
 public class EventApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EventApplication.class, args);
+  }
 
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
 }
